2a2ac6e7aba143c3f373a067e802a621546e0a3e
SamuelPublicover <79486060+SamuelPublicover@users.noreply.github.com>
2021-12-09 08:42:00 -0500

    Pipe's implemented

--- BirdJYSP/ActionScene.cs
+++ BirdJYSP/ActionScene.cs
@@ -26,16 +26,27 @@ namespace BirdJYSP
 
             Random rnd = new Random();
             Vector2 p1pos = new Vector2((Shared.stage.X), (Shared.stage.Y) - rnd.Next(2, (int)Shared.stage.Y));
-            Vector2 p2pos = new Vector2((Shared.stage.X) + 300 , (Shared.stage.Y) - rnd.Next(2, (int)Shared.stage.Y));
-            Vector2 p3pos = new Vector2((Shared.stage.X) + 600, (Shared.stage.Y) - rnd.Next(2, (int)Shared.stage.Y));
-            Pipe pipe1 = new Pipe(game, spriteBatch, birdTex, p1pos);
-            this.Components.Add(pipe1);
+            Vector2 p2pos = new Vector2((Shared.stage.X) + 296 , (Shared.stage.Y) - rnd.Next(2, (int)Shared.stage.Y));
+            Vector2 p3pos = new Vector2((Shared.stage.X) + 593, (Shared.stage.Y) - rnd.Next(2, (int)Shared.stage.Y));
+            Enemy enemy1 = new Enemy(game, spriteBatch, birdTex, p1pos);
+            this.Components.Add(enemy1);
 
-            Pipe pipe2 = new Pipe(game, spriteBatch, birdTex, p2pos);
+            Enemy enemy2 = new Enemy(game, spriteBatch, birdTex, p2pos);
+            this.Components.Add(enemy2);
+
+            Enemy enemy3 = new Enemy(game, spriteBatch, birdTex, p3pos);
+            this.Components.Add(enemy3);
+
+            //width of screen is 800
+            Vector2 pipe1pos = new Vector2((Shared.stage.X), (Shared.stage.Y) - rnd.Next(20, 420));
+            Vector2 pipe2pos = new Vector2((Shared.stage.X) + 400, rnd.Next(-400, -100));
+            Texture2D pipeUpTex = g.Content.Load<Texture2D>("images/PipeUp");
+            Texture2D pipeDownTex = g.Content.Load<Texture2D>("images/PipeDown");
+            Pipe pipe1 = new Pipe(game, spriteBatch, pipeUpTex, pipe1pos);
+            this.Components.Add(pipe1);
+            PipeDown pipe2 = new PipeDown(game, spriteBatch, pipeDownTex, pipe2pos);
             this.Components.Add(pipe2);
 
-            Pipe pipe3 = new Pipe(game, spriteBatch, birdTex, p3pos);
-            this.Components.Add(pipe3);
 
         }
 
--- BirdJYSP/Enemy.cs
+++ BirdJYSP/Enemy.cs
@@ -1,10 +1,71 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace BirdJYSP
 {
-    class Enemy
+    class Enemy : DrawableGameComponent
     {
+        //Variables for an enemy
+        private SpriteBatch spriteBatch;
+        private Texture2D tex;
+        private Vector2 position;
+        private Vector2 stage;
+        private Vector2 pipeSpeed;
+
+        //origin of the object
+        private Vector2 origin;
+        private const int PIPE_OPENING = 100;
+        private Vector2 originalPosition;
+        private static Random rnd = new Random();
+        //width is 800
+        //height is 480
+        private Vector2 startLocation = new Vector2((Shared.stage.X), (Shared.stage.Y) - rnd.Next(10, 470));
+
+        public Enemy(Game game, SpriteBatch spriteBatch, Texture2D tex, Vector2 position) : base(game)
+        {
+            this.spriteBatch = spriteBatch;
+            this.tex = tex;
+            this.position = position;
+            //this.stage = stage;
+            this.pipeSpeed = new Vector2(5, 0);
+            //set origin of pipe to 0,0 (top left)
+            origin = new Vector2(0, 0);
+            originalPosition = position;
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            spriteBatch.Begin();
+            spriteBatch.Draw(tex, position, Color.White);
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            
+            position -= pipeSpeed;
+            
+            //left
+            if(position.X < 0)
+            {
+                Random rnd = new Random();
+                position = startLocation;
+                position.Y = rnd.Next(2, (int)Shared.stage.Y);
+            }
+
+            base.Update(gameTime);
+        }
+
+        public Rectangle getBounds()
+        {
+            return new Rectangle((int)position.X, (int)position.Y, tex.Width, tex.Height);
+        }
+
     }
 }
