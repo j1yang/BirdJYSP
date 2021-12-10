c11ef67f9ef77d9bfb739df927451232e64b8b4a
SamuelPublicover <79486060+SamuelPublicover@users.noreply.github.com>
2021-12-10 15:08:35 -0500

    Committing Changes

--- BirdJYSP/ActionScene.cs
+++ BirdJYSP/ActionScene.cs
@@ -21,7 +21,7 @@ namespace BirdJYSP
 
             //-------------------------------------------------------------------------------
             SoundEffect pointUpSound = g.Content.Load<SoundEffect>("sounds/GetPoint");
-            SoundEffect gunSound = g.Content.Load<SoundEffect>("sounds/gunSound1");
+            SoundEffect gunSound = g.Content.Load<SoundEffect>("sounds/gunSound2");
 
             //-------------------------------------------------------------------------------
             
@@ -38,7 +38,7 @@ namespace BirdJYSP
             
 
             //width of screen is 800
-            Vector2 pipe1pos = new Vector2((Shared.stage.X), (Shared.stage.Y) - rnd.Next(20, 360));
+            Vector2 pipe1pos = new Vector2((Shared.stage.X), (Shared.stage.Y) - rnd.Next(100, 380));
             Vector2 pipe2pos = new Vector2((Shared.stage.X) + 800, rnd.Next(-400, -200));
             Texture2D pipeUpTex = g.Content.Load<Texture2D>("images/PipeUp");
             Texture2D pipeDownTex = g.Content.Load<Texture2D>("images/PipeDown");
