c5c9e8d37bea086238a4b0338bbbdc3b0fe894b4
j1yang <87741372+j1yang@users.noreply.github.com>
2021-12-10 14:54:56 -0500

    score location fixed

--- BirdJYSP/Score.cs
+++ BirdJYSP/Score.cs
@@ -25,7 +25,7 @@ namespace BirdJYSP
             this.spriteBatch = spriteBatch;
 
             this.font = font;
-            pos = new Vector2(Shared.stage.X / 20, Shared.stage.Y / 20);
+            pos = new Vector2(Shared.stage.X - 150, 10);
 
         }
 
